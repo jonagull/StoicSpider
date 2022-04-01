bb091dab7fbc8708a325929fe5d79282084fdedc
Jonathan Gulliksen <jonathangullik@gmail.com>
2022-04-01 15:49:55 +0200

    fixed issue

--- tutorial/spodermane/spiders/quotes_spider.py
+++ tutorial/spodermane/spiders/quotes_spider.py
@@ -15,7 +15,7 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         for quote in response.css('div.quote'):
             yield {
-                'text': quote.css('span.text::text').get(),
+                'text': quote.css('span.text::text').getall(),
                 'author': quote.css('small.author::text').get(),
                 'tags': quote.css('div.tags a.tag::text').getall(),
             }
